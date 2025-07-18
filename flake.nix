7fcb07cadfda8f71bab6ea248a5a7ff3dcf49143
277292 <277292@depeh.de>
2025-07-18 08:28:22 +0200

    Update kirby5 -> 5.0.2

--- flake.nix
+++ flake.nix
@@ -1,38 +1,48 @@
 {
   description = "Kirby CMS Flake";
 
-  outputs = {self, nixpkgs, ...}: {
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  }: {
     overlays.kirby = final: _prev: let
       inherit (final) fetch fetchFromGitHub php83 php84;
       inherit (final.stdenv) mkDerivation;
     in {
-      fetch = {version, rev, sha256, phpPackage}: mkDerivation {
-        pname = "kirby";
-        inherit version;
-        src = fetchFromGitHub {
-          owner = "getkirby";
-          repo = "kirby";
-          inherit rev sha256;
-        };
-      
-        installPhase = ''
-          mkdir -p $out
-          cp -r . $out
-        '';
-
-        meta = {
-          description = "Kirby: the CMS that adapts to any project, loved by developers and editors alike.";
-          homepage = "https://github.com/getkirby/kirby";
-          license = {
-            free = false;
-            redistributable = false;
-            fullName = "While Kirby's source code is publicly available, Kirby is not free. To use Kirby in production, you need to purchase a license.";
-            url = "https://getkirby.com/license";
+      fetch = {
+        version,
+        rev,
+        sha256,
+        phpPackage,
+      }:
+        mkDerivation {
+          pname = "kirby";
+          inherit version;
+          src = fetchFromGitHub {
+            owner = "getkirby";
+            repo = "kirby";
+            inherit rev sha256;
           };
-        };
 
-        passthru = {inherit phpPackage;};
-      };
+          installPhase = ''
+            mkdir -p $out
+            cp -r . $out
+          '';
+
+          meta = {
+            description = "Kirby: the CMS that adapts to any project, loved by developers and editors alike.";
+            homepage = "https://github.com/getkirby/kirby";
+            license = {
+              free = false;
+              redistributable = false;
+              fullName = "While Kirby's source code is publicly available, Kirby is not free. To use Kirby in production, you need to purchase a license.";
+              url = "https://getkirby.com/license";
+            };
+          };
+
+          passthru = {inherit phpPackage;};
+        };
 
       # nix run nixpkgs#nix-prefetch-github -- getKirby kirby --rev <version>
       kirby3 = fetch {
@@ -41,7 +51,7 @@
         sha256 = "sha256-sEPSquK1m3VtN3H1LJ53UY3CO9RLDv9GhYPTRSNSCEE=";
         phpPacakge = php83;
       };
-      
+
       kirby4 = fetch {
         version = "4.8.0";
         rev = "5292c17832dd34b0e5f3e98dea837a357ef037b6";
@@ -50,14 +60,19 @@
       };
 
       kirby5 = fetch {
-        version = "5.0.1";
-        rev = "ddd11f9b4a23e0b5d306b146ca5d6b7a1c1909a9";
-        sha256 = "sha256-JeWQWB/Vtt1vC39Db1HvdACdPmychvpng3n79oO+gd8=";
+        version = "5.0.2";
+        rev = "a57580ac93c643e2e4f84a804bb37b511d445349";
+        sha256 = "sha256-21wfqbfj7Qkl8bmwqf+lVNQPZM0AC5R4XIitbrCUoOA=";
         phpPackage = php84;
       };
     };
-  
-    nixosModules.kirby = {config, lib, pkgs, ...}: let
+
+    nixosModules.kirby = {
+      config,
+      lib,
+      pkgs,
+      ...
+    }: let
       inherit (lib) mkOption mkIf mapAttrs mapAttrs' filterAttrs;
       inherit (lib.types) submodule attrsOf listOf nullOr bool str path package;
 
@@ -123,61 +138,86 @@
       };
 
       config = {
-        users.users = mapAttrs (name: {enable, hostName, root, ...}: mkIf enable {
+        users.users = mapAttrs (name: {
+          enable,
+          hostName,
+          root,
+          ...
+        }:
+          mkIf enable {
             description = "Kirby Instance for ${hostName}";
             isSystemUser = true;
             createHome = true;
             homeMode = "2775";
             home = root;
             group = name;
-        }) cfg.sites;
-        
-        users.groups = mapAttrs (_name: {enable, ...}: mkIf enable {members = ["nginx"];}) cfg.sites;
-        
-        services.phpfpm.pools = mapAttrs (name: {enable, timezone, package, ...}: mkIf enable {
-          user = name;
-          phpPackage = package.phpPackage;
-          settings = {
-            "listen.owner" = config.services.nginx.user;
-            "pm" = "dynamic";
-            "pm.max_children" = 5;
-            "pm.start_servers" = 2;
-            "pm.min_spare_servers" = 1;
-            "pm.max_spare_servers" = 3;
-            "pm.max_requests" = 500;
-            "php_admin_value[error_log]" = "stderr";
-            "php_admin_flag[log_errors]" = true;
-            "catch_workers_output" = true;
-          };
-          phpOptions = ''
-            date.timezone = ${timezone};
-            cgi.fix_pathinfo = 1
-          '';
-        }) cfg.sites;
-        
-        services.nginx.virtualHosts = mapAttrs (name: {enable, hostName, serverAliases, ...}: mkIf enable {
-          forceSSL = true;
-          enableACME = true;
-          serverAliases = ["www.${hostName}"] ++ serverAliases ++ map(domain: "www.${domain}") serverAliases;
-          root = config.users.users.${name}.home;
-          extraConfig = ''
-            index index.php index.html;
-            default_type text/plain;
-            add_header X-Content-Type-Options nosniff;
-            rewrite ^/(content|site|kirby)/(.*)$ /error last;
-            rewrite ^/\.(?!well-known/) /error last;
-            rewrite ^/(?!app\.webmanifest)[^/]+$ /index.php last;
-          '';
-          locations."/".extraConfig = ''try_files $uri $uri/ /index.php$is_args$args;'';
-          locations."~ \.php$".extraConfig = ''
-            try_files $uri =404;
-            fastcgi_split_path_info ^(.+\.php)(/.+)$;
-            fastcgi_pass unix:${config.services.phpfpm.pools.${name}.socket};
-            include ${pkgs.nginx}/conf/fastcgi.conf;
-          '';
-        }) cfg.sites;
+          })
+        cfg.sites;
 
-        fileSystems = mapAttrs' (name: {root, package, ...}: {
+        users.groups = mapAttrs (_name: {enable, ...}: mkIf enable {members = ["nginx"];}) cfg.sites;
+
+        services.phpfpm.pools = mapAttrs (name: {
+          enable,
+          timezone,
+          package,
+          ...
+        }:
+          mkIf enable {
+            user = name;
+            phpPackage = package.phpPackage;
+            settings = {
+              "listen.owner" = config.services.nginx.user;
+              "pm" = "dynamic";
+              "pm.max_children" = 5;
+              "pm.start_servers" = 2;
+              "pm.min_spare_servers" = 1;
+              "pm.max_spare_servers" = 3;
+              "pm.max_requests" = 500;
+              "php_admin_value[error_log]" = "stderr";
+              "php_admin_flag[log_errors]" = true;
+              "catch_workers_output" = true;
+            };
+            phpOptions = ''
+              date.timezone = ${timezone};
+              cgi.fix_pathinfo = 1
+            '';
+          })
+        cfg.sites;
+
+        services.nginx.virtualHosts = mapAttrs (name: {
+          enable,
+          hostName,
+          serverAliases,
+          ...
+        }:
+          mkIf enable {
+            forceSSL = true;
+            enableACME = true;
+            serverAliases = ["www.${hostName}"] ++ serverAliases ++ map (domain: "www.${domain}") serverAliases;
+            root = config.users.users.${name}.home;
+            extraConfig = ''
+              index index.php index.html;
+              default_type text/plain;
+              add_header X-Content-Type-Options nosniff;
+              rewrite ^/(content|site|kirby)/(.*)$ /error last;
+              rewrite ^/\.(?!well-known/) /error last;
+              rewrite ^/(?!app\.webmanifest)[^/]+$ /index.php last;
+            '';
+            locations."/".extraConfig = ''try_files $uri $uri/ /index.php$is_args$args;'';
+            locations."~ \.php$".extraConfig = ''
+              try_files $uri =404;
+              fastcgi_split_path_info ^(.+\.php)(/.+)$;
+              fastcgi_pass unix:${config.services.phpfpm.pools.${name}.socket};
+              include ${pkgs.nginx}/conf/fastcgi.conf;
+            '';
+          })
+        cfg.sites;
+
+        fileSystems = mapAttrs' (name: {
+          root,
+          package,
+          ...
+        }: {
           name = "${root}/kirby"; # config.users.users.${name}.home doesnt work here cause infinite recursion encountered
           value = {
             device = "${package}";
